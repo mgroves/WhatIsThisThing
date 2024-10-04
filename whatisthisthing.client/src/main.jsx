552b215904c6a82c9be01f94a5f9dbbdc5e50184
Matthew D. Groves <me@mgroves.com>
2024-10-04 10:58:32 -0400

    whoops, fix the comment

--- whatisthisthing.client/src/main.jsx
+++ whatisthisthing.client/src/main.jsx
@@ -4,8 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+{/* If you plan to use the admin/auth area, you'll need to put your own Google client ID here (as well as in the backend) */ }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    {/* If you plan to use the admin/auth area, you'll need to put your own Google client ID here (as well as in the backend) */}
     <GoogleOAuthProvider clientId="818950712399-29tikmiav0gkhpim4au86gd02oui5he6.apps.googleusercontent.com">
         <React.StrictMode>
             <App />
