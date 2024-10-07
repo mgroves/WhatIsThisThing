1c236eea9d7f1539bcbec78505450d1f05c979e3
Matthew D. Groves <me@mgroves.com>
2024-10-07 10:58:10 -0400

    client side token expiration

--- whatisthisthing.client/src/components/ProtectedRoute.jsx
+++ whatisthisthing.client/src/components/ProtectedRoute.jsx
@@ -1,16 +1,27 @@
 ﻿import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-function ProtectedRoute({ element: Component, requiredPermission }) {
+function ProtectedRoute({ element: Component }) {
     var auth = JSON.parse(localStorage.getItem('wittAuth'));
 
-    var hasPermission = !(auth == null); // check to make sure auth exists
-
-    if (!hasPermission) {
-        // Redirect to home if no auth loaded
+    if (!auth) {
+        // Redirect to login if no auth loaded
         return <Navigate to="/login" />;
     }
 
+    // Extract the expiration from the top level of auth
+    const expiration = auth.expiration;
+
+    // Check if the current time is past the expiration time
+    const isTokenExpired = Date.now() > expiration;
+
+    if (isTokenExpired) {
+        // Clear out the local storage if the token is expired
+        localStorage.removeItem('wittAuth');
+        return <Navigate to="/login" />;
+    }
+
+    // If token is valid and not expired, render the component
     return <Component />;
 }
 
