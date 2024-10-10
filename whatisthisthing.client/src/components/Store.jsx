a501fc2ef16b6a22297798d3396727a2601e4057
Matthew D. Groves <me@mgroves.com>
2024-10-10 14:24:40 -0400

    added a second map graphic API (CartoDB)

--- whatisthisthing.client/src/components/Store.jsx
+++ whatisthisthing.client/src/components/Store.jsx
@@ -3,8 +3,31 @@ import React, { forwardRef } from 'react';
 const Store = forwardRef(({ store = {} }, ref) => {
     const { name, latitude, longitude } = store;
 
-    // Construct the OpenStreetMap static map URL
-    const imageUrl = `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&z=15&l=map&size=450,450&pt=${longitude},${latitude},pm2rdm`;
+    // CartoDB and Yandex offer ways to get map tiles
+    // without an API key
+
+    const getUrlCartoDb = (latitude, longitude) => {
+        const zoom = 16;
+
+        // convert lon,lat coordinated to carto tile coordinates
+        const lon2tile = (longitude, zoom) => Math.floor((longitude + 180) / 360 * Math.pow(2, zoom));
+        const lat2tile = (latitude, zoom) => {
+            const rad = latitude * Math.PI / 180;
+            return Math.floor((1 - Math.log(Math.tan(rad) + 1 / Math.cos(rad)) / Math.PI) / 2 * Math.pow(2, zoom));
+        };
+
+        const xTile = lon2tile(longitude, zoom);
+        const yTile = lat2tile(latitude, zoom);
+
+        return `https://cartodb-basemaps-a.global.ssl.fastly.net/rastertiles/voyager/${zoom}/${xTile}/${yTile}.png`;
+    };
+
+    const getUrlYandex = (latitude, longitude) => {
+        return `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&z=15&l=map&size=450,450&pt=${longitude},${latitude},pm2rdm`;
+    };
+
+    const imageUrl = getUrlCartoDb(latitude, longitude)
+
     const googleMapsUrl = `https://www.google.com/maps?q=${latitude},${longitude}`;
 
     return (
