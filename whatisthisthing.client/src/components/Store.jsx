786e9d0ec2e0f94d2a8c98377eee5e1d95b8e6e2
Matthew D. Groves <me@mgroves.com>
2024-10-10 14:30:19 -0400

    added two more map APIs for fun

--- whatisthisthing.client/src/components/Store.jsx
+++ whatisthisthing.client/src/components/Store.jsx
@@ -3,8 +3,41 @@ import React, { forwardRef } from 'react';
 const Store = forwardRef(({ store = {} }, ref) => {
     const { name, latitude, longitude } = store;
 
-    // CartoDB and Yandex offer ways to get map tiles
-    // without an API key
+    // multiple map tile providers that don't require an API key
+    const getThunderforestUrl = (latitude, longitude) => {
+        const zoom = 16;
+
+        // Helper functions to convert latitude/longitude to tile numbers
+        const lon2tile = (longitude, zoom) => Math.floor((longitude + 180) / 360 * Math.pow(2, zoom));
+        const lat2tile = (latitude, zoom) => {
+            const rad = latitude * Math.PI / 180;
+            return Math.floor((1 - Math.log(Math.tan(rad) + 1 / Math.cos(rad)) / Math.PI) / 2 * Math.pow(2, zoom));
+        };
+
+        // Calculate tile numbers
+        const xTile = lon2tile(longitude, zoom);
+        const yTile = lat2tile(latitude, zoom);
+
+        // Construct the Thunderforest URL
+        return `https://tile.thunderforest.com/cycle/${zoom}/${xTile}/${yTile}.png`;
+    };
+
+    const getWikimediaUrl = (latitude, longitude) => {
+        const zoom = 16;
+
+        const lon2tile = (longitude, zoom) => Math.floor((longitude + 180) / 360 * Math.pow(2, zoom));
+        const lat2tile = (latitude, zoom) => {
+            const rad = latitude * Math.PI / 180;
+            return Math.floor((1 - Math.log(Math.tan(rad) + 1 / Math.cos(rad)) / Math.PI) / 2 * Math.pow(2, zoom));
+        };
+
+        // Calculate tile numbers
+        const xTile = lon2tile(longitude, zoom);
+        const yTile = lat2tile(latitude, zoom);
+
+        // Construct the Wikimedia URL
+        return `https://maps.wikimedia.org/osm-intl/${zoom}/${xTile}/${yTile}.png`;
+    };
 
     const getUrlCartoDb = (latitude, longitude) => {
         const zoom = 16;
@@ -26,7 +59,7 @@ const Store = forwardRef(({ store = {} }, ref) => {
         return `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&z=15&l=map&size=450,450&pt=${longitude},${latitude},pm2rdm`;
     };
 
-    const imageUrl = getUrlCartoDb(latitude, longitude)
+    const imageUrl = getWikimediaUrl(latitude, longitude)
 
     const googleMapsUrl = `https://www.google.com/maps?q=${latitude},${longitude}`;
 
